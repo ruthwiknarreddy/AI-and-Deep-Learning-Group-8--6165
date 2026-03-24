f5507b7daab9cf9d5455e859eee077bb8bd5410f
Abeoseh Flemister <aflemis1@charlotte.com>
2026-03-23 21:34:53 -0400

    switch to pytorch

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 conda env create -f ./environment.yml
-jupyter contrib nbextension install --user
+#jupyter contrib nbextension install --user
 python -m ipykernel install --user --name jupyter_env --display-name "Python (deep_learning)"
